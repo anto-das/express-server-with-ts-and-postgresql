938891763309ce7afd15b0990a33bb636c19e7cc
unknown <antuvhai@gmail.com>
2025-12-03 14:23:09 +0600

    apply jwt in users get api

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,19 +1,26 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { config } from "../config";
 
 const auth = () => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    if (!token) {
-      return res.status(500).send({
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(500).send({
+          success: false,
+          message: "Your not allowed",
+        });
+      }
+      const decoded = jwt.verify(token, config.jwt_secret as string);
+      req.user = decoded as JwtPayload;
+      next();
+    } catch (err: any) {
+      res.status(500).send({
         success: false,
-        message: "Your not allowed",
+        message: err.message,
       });
     }
-    const decoded = jwt.verify(token,config.jwt_secret as string)
-    console.log({decoded:decoded})
-    next();
   };
 };
 
